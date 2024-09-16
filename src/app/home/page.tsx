17d14e5703cb0dda02595ea9b5bd2f77513d8ae3
Robert Fidler <robertf224@gmail.com>
2024-09-16 14:00:21 -0400

    weird bugfix

---
+++ src/app/home/page.tsx
@@ -0,0 +1,6 @@
+import { HomeView } from "@/components/home-view";
+
+// TODO: understand why we need this page for the real home page to not break...
+export default async function HomePage() {
+  return <HomeView />;
+}
