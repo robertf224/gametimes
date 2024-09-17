13c59261981edb10c800710b16aea046c6535869
Robert Fidler <robertf224@gmail.com>
2024-09-17 15:15:03 -0400

    fix loading bugs

--- src/logic/foundryClient.ts
+++ src/logic/foundryClient.ts
@@ -10,7 +10,9 @@ function checkEnv(
   }
 }
 
-function createFoundryClient(): Client {
+// TODO: fix this so we can have a better way to store refresh token
+// https://github.com/palantir/osdk-ts/blob/3c05ea607bada4eec2b8f7480c3183cbd7fec444/packages/oauth/src/common.ts
+export const createFoundryClient = (): Client => {
   checkEnv(process.env.FOUNDRY_CLIENT_ID, "FOUNDRY_CLIENT_ID");
   checkEnv(process.env.FOUNDRY_CLIENT_SECRET, "FOUNDRY_CLIENT_SECRET");
   checkEnv(process.env.FOUNDRY_URL, "FOUNDRY_URL");
@@ -19,7 +21,10 @@ function createFoundryClient(): Client {
   const auth = createConfidentialOauthClient(
     process.env.FOUNDRY_CLIENT_ID,
     process.env.FOUNDRY_CLIENT_SECRET,
-    process.env.FOUNDRY_URL
+    process.env.FOUNDRY_URL,
+    undefined,
+    // TODO: maybe push this upstream into OSDK, we need to do this so auth doesn't break
+    (url, options) => fetch(url, { ...options, cache: "no-store" })
   );
 
   return createClient(
@@ -27,17 +32,6 @@ function createFoundryClient(): Client {
     process.env.FOUNDRY_ONTOLOGY_RID,
     auth
   );
-}
-
-// Implementing this so refresh token gets stored
-// https://github.com/palantir/osdk-ts/blob/3c05ea607bada4eec2b8f7480c3183cbd7fec444/packages/oauth/src/common.ts
-const storage = new Map<string, string>();
-globalThis.localStorage = {
-  setItem: (key, value) => storage.set(key, value),
-  getItem: (key) => storage.get(key) ?? null,
-  removeItem: (key) => storage.delete(key),
-  clear: () => storage.clear(),
-  key: (index) => Array.from(storage.keys())[index],
-  length: 0,
 };
+
 export const foundryClient = createFoundryClient();
